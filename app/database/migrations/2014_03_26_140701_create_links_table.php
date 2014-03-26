639475537e0d57be7e4c8de0c917c3a64646dd9e
David Sivocha <david@sivocha.com>
2014-03-26 14:40:17 +0000

    Corrected migration from ::table to ::create

--- app/database/migrations/2014_03_26_140701_create_links_table.php
+++ app/database/migrations/2014_03_26_140701_create_links_table.php
@@ -12,7 +12,7 @@ class CreateLinksTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::table('links', function(Blueprint $table)
+		Schema::create('links', function(Blueprint $table)
 		{
 			//
 			$table->increments('id');
