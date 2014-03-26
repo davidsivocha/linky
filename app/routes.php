836bfbaf0f1933cde4504aa142476c24d0736731
David Sivocha <david@sivocha.com>
2014-03-26 14:32:47 +0000

    Add a post route for the URL Shortener

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,34 @@ Route::get('/', function()
 {
 	return View::make('form');
 });
+
+Route::post('/', function(){
+	//First define validation rules
+	$rules = array(
+		'link' => 'required|url'
+	);
+
+	//Run form validation
+	$validation = Validator::make(Inpute::all(),$rules);
+
+	//if Validation fails return to '/' with an error
+	if($validation->fails()){
+		return Redirect::to('/')->withInput()->withErrors($validation);
+	} else {
+		//Check if link already in DB
+		$link = Link::where('url','=',Input::get('link'))->first();
+		//If we have it return it
+		if($link){
+			return Redirect::to('/')->withInput()->with('link',$link->hash);
+		} else {
+			do {
+				$newHash = Str::random(6);
+			} while(Link::where('hash','=',$newHash)->count() > 0);
+			Link::create(array(
+				'url' => Input::get('link'),
+				'hash' => $newHash
+			));
+			return Redirect::to('/')->withInput()->with('link', $newHash);
+		}
+	}
+});
